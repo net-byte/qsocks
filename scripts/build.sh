8aca3da4621fa6b0999bf7b0c2bf7e695e32caa8
netbyte <alex.tsai@protonmail.com>
2022-06-09 21:46:09 +0800

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,17 @@
 #!bin/bash
-#Linux
+#Linux amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/qsocks-linux-amd64 ./main.go
-#Linux arm
+#Linux arm64
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o ./bin/qsocks-linux-arm64 ./main.go
-#Mac OS
+#MacOS amd64
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./bin/qsocks-darwin-amd64 ./main.go
-#Windows
+#MacOS arm64
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ./bin/qsocks-darwin-arm64 ./main.go
+#Windows amd64
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./bin/qsocks-windows-amd64.exe ./main.go
-#Operwrt
+#Windows arm64
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -o ./bin/qsocks-windows-arm64.exe ./main.go
+#OperWRT amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags="-s -w" -o ./bin/qsocks-openwrt-amd64 ./main.go
 
 echo "DONE!!!"
